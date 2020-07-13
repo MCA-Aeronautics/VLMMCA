eedfcb083a9a81ae1a7477ebb78946f4d1dfc521
Mark-C-Anderson <anderson.mark.az@gmail.com>
2020-07-13 10:37:05 -0600

    used norm(freestream) in cl calcs

--- src/VLMMCA.jl
+++ src/VLMMCA.jl
@@ -477,6 +477,7 @@ module VLMMCA
             deltaY = abs(panels[i,2] - panels[i,5])
             
             Lift = Lift + density*norm(freestream[i,:])*GammaValues[i]*deltaY;
+            #Lift = Lift + density*norm(freestream[1,:])*GammaValues[i]*deltaY;
             
         end
         
@@ -508,7 +509,7 @@ module VLMMCA
             Area = Area + incrementalArea
             
             # See eqn 7.51 in Bertin's book
-            cl[i] = (density.*freestream[i]*GammaValues[i]*deltaY)/(dynamicPressure * incrementalArea) # if unit span is only in y
+            cl[i] = (density.*norm(freestream[i,:])*GammaValues[i]*deltaY)/(dynamicPressure * incrementalArea) # if unit span is only in y
             #Cl[i] = (freestream[i]*GammaValues[i]*deltaY)/(dynamicPressure * deltaX * panelFrontLength) # if unit span is along the leading edge
             clSpanLocations[i] = panels[i,2] + deltaY/2
 
@@ -553,7 +554,8 @@ module VLMMCA
     
         end
 
-        # figure()
+        # figure(10)
+        # clf()
         # plotPanels(panels)
         # scatter3D(boundVortexCenters[:,1],boundVortexCenters[:,2],boundVortexCenters[:,3],color = "black")
     
